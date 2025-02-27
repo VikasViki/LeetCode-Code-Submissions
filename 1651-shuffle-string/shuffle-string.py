d651be68f8f05fb9e7fdb645861a5347c14833d0
Vikas Viki <vikasviki489@gmail.com>
2025-02-28 02:26:02 +0530

    Time: 3 ms (32.62%) | Memory: 17.9 MB (24.79%) - LeetSync

--- 1651-shuffle-string/shuffle-string.py
+++ 1651-shuffle-string/shuffle-string.py
@@ -1,9 +1,19 @@
+from typing import List
+
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        s_len = len(s)
-        shuffled_string = [''] * s_len
-        for s_index, shuffled_index in enumerate(indices):
-            curr_char = s[s_index]
-            shuffled_string[shuffled_index] = curr_char
-        return "".join(shuffled_string)
+        s = list(s)  # Convert to a mutable list
         
+        for i in range(len(s)):
+            while indices[i] != i:
+                target_idx = indices[i]
+                
+                # Swap characters
+                s[i], s[target_idx] = s[target_idx], s[i]
+                
+                # Swap indices to reflect the swap
+                indices[i], indices[target_idx] = indices[target_idx], indices[i]
+
+                # print(i, indices[i])
+        
+        return "".join(s)  # Convert back to string
