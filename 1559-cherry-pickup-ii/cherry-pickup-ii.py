02a25b7abf8b43fde90fe0895e923f9d3cf37611
Vikas Viki <vikasviki489@gmail.com>
2025-03-02 03:25:23 +0530

    Time: 2364 ms (7.34%) | Memory: 37 MB (34.00%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+
+    def is_out_of_bound(self, row, col):
+        return (not (0 <= row < self.total_rows)) or (not (0 <= col < self.total_cols))
+
+    def get_max_cherries(self, r1_row, r1_col, r2_row, r2_col, r1_score, r2_score):
+        if self.is_out_of_bound(r1_row, r1_col):
+            return 0
+        if self.is_out_of_bound(r2_row, r2_col):
+            return 0
+        
+        key = (r1_row, r1_col, r2_row, r2_col)
+
+        if key in self.memo:
+            return self.memo[key]
+
+        if (r1_row == self.total_rows and r2_row == total_cols):
+            return r1_score + r2_score
+        
+        max_score = 0
+
+        for new_r1_col in [r1_col-1, r1_col, r1_col+1]:
+            for new_r2_col in [r2_col-1, r2_col, r2_col+1]:
+                r1_cell = self.grid[r1_row][r1_col]
+                r2_cell = self.grid[r2_row][r2_col] if (r1_row, r1_col) != (r2_row, r2_col) else 0
+                curr_score = r1_cell + r2_cell + \
+                self.get_max_cherries(r1_row+1, new_r1_col, r2_row+1, new_r2_col, r1_score, r2_score)
+                max_score = max(curr_score, max_score)
+        
+        self.memo[key] = max_score
+        
+        return max_score
+
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        self.memo = {}
+        self.grid = grid
+        self.total_rows = len(self.grid)
+        self.total_cols = len(self.grid[0])
+        max_score = self.get_max_cherries(0, 0, 0, self.total_cols-1, 0, 0)
+        return max_score
