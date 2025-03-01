473d1cc1d7723a4432730669c552b7d76e374647
Vikas Viki <vikasviki489@gmail.com>
2025-03-01 05:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (74.68%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,19 +1,19 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        # Updating nums
         nums_len = len(nums)
-        for index in range(nums_len-1):
-            if nums[index] == nums[index+1]:
-                nums[index] = nums[index+1] * 2
-                nums[index+1] = 0
-        
-        # moving zeros
         new_nums = [0] * nums_len
-        new_nums_index = 0
+        new_num_index = 0
+
         for index in range(nums_len):
-            if nums[index] != 0:
-                new_nums[new_nums_index] = nums[index]
-                new_nums_index += 1
-        
+            if index+1 < nums_len and nums[index] == nums[index+1]:
+                if nums[index] == 0: continue
+                new_nums[new_num_index] = nums[index] * 2
+                nums[index+1] = 0
+                new_num_index += 1
+            else:
+                if nums[index] != 0:
+                    new_nums[new_num_index] = nums[index]
+                    new_num_index += 1
+
         return new_nums
 
