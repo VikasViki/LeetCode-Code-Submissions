823fcf86612b8d07be06de51f11113389f6cf8b9
Vikas Viki <vikasviki489@gmail.com>
2025-03-04 23:43:52 +0530

    Time: 125 ms (34.47%) | Memory: 36.2 MB (86.99%) - LeetSync

--- 2483-task-scheduler-ii/task-scheduler-ii.py
+++ 2483-task-scheduler-ii/task-scheduler-ii.py
@@ -1,17 +1,10 @@
 class Solution:
     def taskSchedulerII(self, tasks: List[int], space: int) -> int:
-        task_executed_day = {}
-        curr_day = 0
-
+        day = 1
+        execution_day = {}
         for task in tasks:
-            if task not in task_executed_day:
-                task_executed_day[task] = curr_day
-                curr_day += 1
-            else:
-                prev_task_executed_day = task_executed_day[task]
-                new_task_executed_day = prev_task_executed_day + space + 1
-                curr_day = max(new_task_executed_day, curr_day)
-                task_executed_day[task] = curr_day
-                curr_day += 1
-            
-        return curr_day
+            if task in execution_day:
+                day = max(day, execution_day[task] + space + 1)
+            execution_day[task] = day
+            day += 1
+        return day - 1
