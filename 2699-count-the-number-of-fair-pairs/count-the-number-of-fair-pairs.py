19905067bd666d2616710edea2780dc7ad1c1b4b
Vikas Viki <vikasviki489@gmail.com>
2025-04-19 11:11:07 +0530

    Time: 268 ms (36.98%) | Memory: 31.6 MB (19.86%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -1,23 +1,14 @@
 class Solution:
 
-    def lower_bound_binary_search(self, nums, low, high, target):
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid] >= target:
-                high = mid - 1
-            else:
-                low = mid + 1
-        return low
-
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
         fair_pairs = 0
         nums_len = len(nums)
-        for index, num in enumerate(nums):
+        for i, num in enumerate(nums):
             lower_target = lower - num
-            left_index = self.lower_bound_binary_search(nums, index+1, nums_len-1, lower_target)
+            left_index = bisect_left(nums, lower_target, lo=i+1)
             upper_target = upper - num + 1
-            right_index = self.lower_bound_binary_search(nums, index+1, nums_len-1, upper_target)
+            right_index = bisect_left(nums, upper_target, lo=i+1)
             fair_pairs += right_index - left_index
         
         return fair_pairs
