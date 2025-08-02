66c814fbdc5573e6f09236d16cefcb6af0452c0e
Vikas Viki <vikasviki489@gmail.com>
2025-08-02 22:24:50 +0530

    Time: 265 ms (28.83%) | Memory: 19.7 MB (15.00%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -3,7 +3,7 @@ class Solution:
     def find_substring_count(self, string, substring):
         if not string:
             return 0, ""
-            
+
         stack = [string[0]]
         substring_count = 0
         for char in string[1:]:
@@ -18,14 +18,13 @@ class Solution:
 
 
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        # ab count
-        ab_count, string = self.find_substring_count(s, 'ab')
-        ba_count, string = self.find_substring_count(string, 'ba')
-        score1 = ab_count * x + ba_count * y
-
-        ba_count, string = self.find_substring_count(s, 'ba')
-        ab_count, string = self.find_substring_count(string, 'ab')
-        score2 = ba_count * y + ab_count * x
-
-        return max(score1, score2)
-
+        if x > y:
+            # ab count
+            ab_count, string = self.find_substring_count(s, 'ab')
+            ba_count, string = self.find_substring_count(string, 'ba')
+            return ab_count * x + ba_count * y
+        else:
+            # ba count
+            ba_count, string = self.find_substring_count(s, 'ba')
+            ab_count, string = self.find_substring_count(string, 'ab')
+            return ba_count * y + ab_count * x
