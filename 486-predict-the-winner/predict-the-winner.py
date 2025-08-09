03ede0de74cbb4e9ef7f0cebfb3a9e8aca9fe7e6
Vikas Viki <vikasviki489@gmail.com>
2025-08-09 22:29:33 +0530

    Time: 3 ms (68.47%) | Memory: 17.7 MB (73.16%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,15 +1,21 @@
 class Solution:
 
     def get_score(self, start, end):
+        if (start, end) in self.memo:
+            return self.memo[(start, end)]
+
         if start > end:
             return 0
         
         pick_start = self.nums[start] - self.get_score(start+1, end)
         pick_end = self.nums[end] - self.get_score(start, end-1)
 
-        return max(pick_start, pick_end)
+        self.memo[(start, end)] = max(pick_start, pick_end)
+
+        return self.memo[(start, end)]
 
     def predictTheWinner(self, nums: List[int]) -> bool:
+        self.memo = {}
         self.nums = nums
         start, end = 0, len(nums)-1
         score = self.get_score(start, end)
