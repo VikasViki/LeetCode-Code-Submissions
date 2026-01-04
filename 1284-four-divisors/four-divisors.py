0b9d05783f213729f364757d2741eabb0a0c1941
Vikas Viki <vikasviki489@gmail.com>
2026-01-04 20:54:31 +0530

    Time: 44 ms (98.61%) | Memory: 21.5 MB (5.23%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.py
@@ -0,0 +1,35 @@
+import math
+def get_four_divisors_sum(num):
+    divisor_sum = 0
+    divisor_found = False
+    limit = floor(sqrt(num))
+    # print(num, limit)
+    for divisor in range(2, limit+1):
+        if num % divisor == 0:
+            if divisor_found:
+                return 0
+            if divisor != (num // divisor):
+                divisor_sum += divisor
+                divisor_sum += (num // divisor)
+                divisor_found = True
+            else:
+                return 0
+    
+    if divisor_found:
+        return divisor_sum + 1 + num
+    else:
+        return 0
+
+four_divisors_sum = {}
+for num in range(2, 100001):
+    divisors_sum = get_four_divisors_sum(num)
+    if divisors_sum != 0:
+        four_divisors_sum[num] = divisors_sum
+
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        print(four_divisors_sum)
+        sum_of_divisors = 0
+        for num in nums:
+            sum_of_divisors += four_divisors_sum.get(num, 0)
+        return sum_of_divisors
